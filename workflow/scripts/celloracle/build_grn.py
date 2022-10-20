8b1265dab6953cc6d36e3edc1999fe41573e1103
demian1 <robin.fallegger@googlemail.com>
2022-10-20 10:07:52 +0200

    build grn updated for astromice

--- workflow/scripts/celloracle/build_grn.py
+++ workflow/scripts/celloracle/build_grn.py
@@ -27,15 +27,24 @@ base_GRN = pd.read_csv(path_base_grn, index_col=0)
 # Instantiate Oracle object
 oracle = co.Oracle()
 
-# Set to raw counts
-adata.X = adata.layers['counts']
-
-# Filter by HVG
-adata = adata[:, adata.var.highly_variable]
+# Find HVG
+adata.layers['counts'] = adata.X.copy()
+sc.pp.normalize_total(adata, target_sum=1e4)
+sc.pp.log1p(adata)
+sc.pp.highly_variable_genes(adata, batch_key='bio_origin')
+adata.var = adata.var[['highly_variable','highly_variable_nbatches']]
+num_hvg_genes = 3000
+batch_msk = np.array(adata.var.highly_variable_nbatches > 1)
+hvg = adata.var[batch_msk].sort_values('highly_variable_nbatches').tail(num_hvg_genes).index
+adata.var['highly_variable'] = [g in hvg for g in adata.var.index]
+adata.var = adata.var[['highly_variable','highly_variable_nbatches']]
+adata = adata[:,hvg]
+adata.X = adata.layers['counts'].copy()
+adata.obs['tissue'] = 'tissue'
 
 # Instantiate Oracle object
 oracle.import_anndata_as_raw_count(adata=adata,
-                                   cluster_column_name="celltype",
+                                   cluster_column_name="tissue",
                                    embedding_name="X_umap")
 
 # You can load TF info dataframe with the following code
@@ -55,7 +64,7 @@ oracle.knn_imputation(n_pca_dims=n_comps, k=k, balanced=True, b_sight=k*8,
                       b_maxl=k*4, n_jobs=os.cpu_count())
 
 # Compute GRN
-links = oracle.get_links(cluster_name_for_GRN_unit="celltype", alpha=10,
+links = oracle.get_links(cluster_name_for_GRN_unit="tissue", alpha=10,
                          verbose_level=10)
 
 # Save Links object
